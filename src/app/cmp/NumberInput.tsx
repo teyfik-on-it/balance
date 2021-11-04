7802677efe408119a6f1bb0e48add81de0c778c1
Halil Teyfik <49863255+teyfik-on-it@users.noreply.github.com>
2021-11-04 14:06:39 +0300

    removed redundant console.log

--- src/app/cmp/NumberInput.tsx
+++ src/app/cmp/NumberInput.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import NumberFormat from 'react-number-format';
 import './NumberInput.scss';
 
@@ -19,29 +18,24 @@ interface Props {
  * NumberFormat wrapper with project specific custom options
  * Can be used as same as an input with type "number"
  */
-const NumberInput = ({ value, onChange, prefix, invalid }: Props) => {
-  useEffect(() => console.log(prefix + ' value', value), [value, prefix]);
-
-  return (
-    <NumberFormat
-      value={value}
-      prefix={prefix + ' '}
-      decimalScale={6}
-      allowNegative={false}
-      allowEmptyFormatting={false}
-      allowLeadingZeros={false}
-      decimalSeparator={','}
-      thousandSeparator={'.'}
-      fixedDecimalScale={false}
-      onValueChange={({ floatValue }) => {
-        if ('function' === typeof onChange && 'number' === typeof floatValue) {
-          onChange(floatValue);
-          console.log(prefix + ' floatValue', floatValue);
-        }
-      }}
-      className={'number-input ' + (invalid ? 'invalid ' : '')}
-    />
-  );
-};
+const NumberInput = ({ value, onChange, prefix, invalid }: Props) => (
+  <NumberFormat
+    value={value}
+    prefix={prefix + ' '}
+    decimalScale={6}
+    allowNegative={false}
+    allowEmptyFormatting={false}
+    allowLeadingZeros={false}
+    decimalSeparator={','}
+    thousandSeparator={'.'}
+    fixedDecimalScale={false}
+    onValueChange={({ floatValue }) => {
+      if ('function' === typeof onChange && 'number' === typeof floatValue) {
+        onChange(floatValue);
+      }
+    }}
+    className={'number-input ' + (invalid ? 'invalid ' : '')}
+  />
+);
 
 export default NumberInput;
